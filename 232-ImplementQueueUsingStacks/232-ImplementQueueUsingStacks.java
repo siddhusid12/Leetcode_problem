1b16299327bcfd4cc44a05e82ffd5273e0871a21
Siddharth <162794644+siddhusid12@users.noreply.github.com>
2025-09-03 10:08:21 +0530

    Create 232-ImplementQueueUsingStacks

---
+++ 232-ImplementQueueUsingStacks/232-ImplementQueueUsingStacks.java
@@ -0,0 +1,25 @@
+// Last updated: 9/3/2025, 10:08:14 AM
+class MyQueue {
+  public void push(int x) {
+    input.push(x);
+  }
+
+  public int pop() {
+    peek();
+    return output.pop();
+  }
+
+  public int peek() {
+    if (output.isEmpty())
+      while (!input.isEmpty())
+        output.push(input.pop());
+    return output.peek();
+  }
+
+  public boolean empty() {
+    return input.isEmpty() && output.isEmpty();
+  }
+
+  private Deque<Integer> input = new ArrayDeque<>();
+  private Deque<Integer> output = new ArrayDeque<>();
+}
