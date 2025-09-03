bcc1e2545097b0165bd14660c4bdb34959f05750
Siddharth <162794644+siddhusid12@users.noreply.github.com>
2025-09-03 10:08:46 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,20 @@
+// Last updated: 9/3/2025, 10:08:39 AM
+class Solution {
+  public boolean isPalindrome(String s) {
+    int l = 0;
+    int r = s.length() - 1;
+
+    while (l < r) {
+      while (l < r && !Character.isLetterOrDigit(s.charAt(l)))
+        ++l;
+      while (l < r && !Character.isLetterOrDigit(s.charAt(r)))
+        --r;
+      if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r)))
+        return false;
+      ++l;
+      --r;
+    }
+
+    return true;
+  }
+}
