e62e3e970dc23b7efa153ec3b617f10da989c96e
Siddharth <162794644+siddhusid12@users.noreply.github.com>
2025-09-03 10:09:02 +0530

    Create 99-RecoverBinarySearchTree

---
+++ 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.java
@@ -0,0 +1,37 @@
+// Last updated: 9/3/2025, 10:08:55 AM
+class Solution {
+    private TreeNode first = null;
+    private TreeNode second = null;
+    private TreeNode pre = null;
+    
+    public void recoverTree(TreeNode root) {
+        if(root==null) return;
+
+        //find swapped nodes
+        inorder(root);
+        
+        //swap the nodes
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+    
+    private void inorder(TreeNode root){
+        if(root==null) return;
+        inorder(root.left);
+        
+        //find the first node which is at wrong position
+        if(first==null && (pre==null ||pre.val>=root.val)){
+            first = pre;
+        }
+        
+        //find the second node which is at wrong position
+        if(first!=null && pre.val>=root.val){
+            second = root;
+        }
+        
+        //store previus node to compare
+        pre = root;
+        inorder(root.right);
+    }
+}
