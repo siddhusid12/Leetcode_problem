671a851a317143f21a201d4e7877e0c58b56ded1
Siddharth <162794644+siddhusid12@users.noreply.github.com>
2025-09-03 10:07:49 +0530

    Create 3490-FindTheMaximumLengthOfValidSubsequenceI

---
+++ 3490-FindTheMaximumLengthOfValidSubsequenceI/3490-FindTheMaximumLengthOfValidSubsequenceI.java
@@ -0,0 +1,18 @@
+// Last updated: 9/3/2025, 10:07:43 AM
+class Solution {
+    public int maximumLength(int[] nums) {
+        int c = nums[0] % 2, odd = 0, even = 0, both = 0;
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+            if (num % 2 == c) {
+                both++;
+                c = 1 - c;  
+            }
+        }
+        return Math.max(both, Math.max(even, odd));
+    }
+}
